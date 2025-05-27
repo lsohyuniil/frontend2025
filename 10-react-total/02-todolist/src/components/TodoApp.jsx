c0f1c1aa3439331c813329c2ddaade272c68b7ff
lsohyuniil <lim07so10h@gmail.com>
2025-05-27 11:21:35 +0900

    todolist (useContext, localStorage)

---
+++ 10-react-total/02-todolist/src/components/TodoApp.jsx
@@ -0,0 +1,17 @@
+import { TodoProvider } from "../context/TodoContext";
+import TodoCount from "./TodoCount";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+
+export default function TodoApp() {
+  return (
+    <TodoProvider>
+      <div className="todoApp">
+        <h1>Todo List</h1>
+        <TodoForm />
+        <TodoList />
+        <TodoCount />
+      </div>
+    </TodoProvider>
+  );
+}
